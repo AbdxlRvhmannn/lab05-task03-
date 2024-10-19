7e7dbca76ba281dc1a80aa3448cdc0a51390b10d
Abdul Rehman <abdulr661@gmail.com>
2024-10-19 22:27:33 +0500

    Add files via upload

---
+++ main.dart
@@ -0,0 +1,102 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: FirstScreen(),
+    );
+  }
+}
+
+class FirstScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('First Screen')),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Hero(
+              tag: 'hero-image',
+              child: Image.asset(
+                'assets/rdr2.png',
+                width: 150,
+                height: 150,
+              ),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondScreen()),
+                );
+              },
+              child: Text('Go to Second Screen'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Screen')),
+      body: Center(
+        child: Hero(
+          tag: 'hero-image',
+          child: Image.asset(
+            'assets/rdr2.png',
+            width: 300,
+            height: 300,
+          ),
+        ),
+      ),
+    );
+  }
+}
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Explicit Animation')),
+//         body: AnimatedControllerWidget(),
+//       ),
+//     );
+//   }
+// }
+// import 'AbdulRehman.dart';
+// import 'pp_01.dart';
+// import 'animated_container_widget.dart';
+//
+// void main() {
+//   runApp(MyApp());
+// }
+//
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(title: Text('Animated Container ')),
+//         body: AnimatedContainerWidget(),
+//       ),
+//     );
+//   }
+// }
+
